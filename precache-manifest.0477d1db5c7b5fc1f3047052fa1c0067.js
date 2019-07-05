9ba7045a7950e9383ecef94ba6e7b0b4c549b508
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-07-05 15:47:36 +0000

    Deploy vshaddix/reactjs-assessment to github.com/vshaddix/reactjs-assessment.git:gh-pages

---
+++ precache-manifest.0477d1db5c7b5fc1f3047052fa1c0067.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc8837d287c97febd9f06ac6eaf54b7d",
+    "url": "/reactjs-assessment/index.html"
+  },
+  {
+    "revision": "4a34120f5a9702b77286",
+    "url": "/reactjs-assessment/static/css/main.3c715ce7.chunk.css"
+  },
+  {
+    "revision": "69f54f7e212b9b5abdd9",
+    "url": "/reactjs-assessment/static/js/2.7b31dd15.chunk.js"
+  },
+  {
+    "revision": "4a34120f5a9702b77286",
+    "url": "/reactjs-assessment/static/js/main.bd0f1003.chunk.js"
+  },
+  {
+    "revision": "30aee650f3438e294a15",
+    "url": "/reactjs-assessment/static/js/runtime~main.2f2eafbf.js"
+  }
+]);
