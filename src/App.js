1925a020e4c11ed6d09cb510f2af91e2eb3fcd13
Vasil Shaddix <vasilshaddixx@gmail.com>
2019-07-05 12:04:50 +0300

    remove the app css and refactor the base app component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import OpenAQService from './services/OpenAQService';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import MeasurementCell from './components/MeasurementCell/MeasurementCell';
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = { cities: [], singleMeasurements: { parameter: null, value: null, unit: null } };
+  }
+
+  async componentDidMount() {
+    const service = new OpenAQService();
+
+    let cities = await service.getCities();
+    if (cities.meta && cities.meta.found > 100) {
+      cities = await service.getCities({ limit: cities.meta.found });
+    }
+
+    const firstCity = cities.results[0];
+    const cityMeasurements = await service.getLatestMeasurements({ city: firstCity.city, country: firstCity.country});
+    this.setState({
+      cities: cities.results,
+      singleMeasurements: cityMeasurements.results[0].measurements[0]
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <MeasurementCell
+            parameter={this.state.singleMeasurements.parameter}
+            unit={this.state.singleMeasurements.unit}
+            value={this.state.singleMeasurements.value}
+          />
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
